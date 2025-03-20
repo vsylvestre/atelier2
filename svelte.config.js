d7c9cbd4f2f97a39b4d4a73b137958f64c05a108
Vincent Sylvestre <vinc.sylvestre@gmail.com>
2025-03-20 10:30:24 -0400

    Adjust Svelte config for GitHub actions

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,14 @@ import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const IS_DEV = process.argv.includes('dev');
+const BASE_PATH = IS_DEV ? '' : process.env.BASE_PATH || '';
+
+const getEntries = () => {
+	const entries = ['/', '/1', '/2', '/3', '/4', '/5', '/6', '/7', '/8'];
+	return BASE_PATH ? entries.map((entry) => `/${BASE_PATH}${entry}`) : entries;
+};
+
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
@@ -10,10 +18,10 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: BASE_PATH
 		},
 		prerender: {
-			entries: ['/', '/1', '/2', '/3', '/4', '/5', '/6', '/7', '/8']
+			entries: getEntries()
 		}
 	}
 };
