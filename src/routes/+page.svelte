f24c9addb9f4f456b06de7aa78323b5aa7b56589
Vincent Sylvestre <vinc.sylvestre@gmail.com>
2025-03-21 11:55:00 -0400

    Add favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<img src="/intro.jpg" class="size-full min-h-screen object-cover px-64 bp:px-100" alt="QR codes" />
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<img
+	src="{base}/intro.jpg"
+	class="size-full min-h-screen object-cover px-64 bp:px-100"
+	alt="QR codes"
+/>
